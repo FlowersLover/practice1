7894f7eb6fb50c1ecc9d25280ffd32edf2060acc
Murashkina <grewytri33@gmail.com>
2022-11-19 19:40:13 +0400

    update PersonTest

--- src/main/java/data_types/PersonTest.java
+++ src/main/java/data_types/PersonTest.java
@@ -6,29 +6,44 @@ import static org.testng.Assert.*;
 
 public class PersonTest {
     @Test
-    public void testCase(){
+    public void testFirstName(){
         Person person = new Person();
         person.setFirstName("Dasha");
-        person.setLastName("Murashkina");
-        person.setPasssportId(1);
-        Person person2 = new Person();
-        person2.setFirstName("Alexandr");
-        person2.setLastName("Tsarkov");
-        person2.setPasssportId(2);
+        Person person1 = new Person();
+        person1.setFirstName("Alexandr");
+        Person person2 = new Person("Natali","Murashkina",2);
         Person person3 = new Person();
-        person3.setFirstName("Natali");
-        person3.setLastName("Murashkina");
-        person3.setPasssportId(3);
+        person3.setFirstName(null);
         Person person4 = new Person();
-        person4.setFirstName(" ");
-        person4.setLastName(" ");
-        person4.setPasssportId(100065);
-        assertEquals(person3.getFirstName(),"Natali");
-        assertEquals(person4.getPasssportId(),100065);
-        assertEquals(person.getPasssportId(),1);
-        assertEquals(person4.getLastName()," ");
-
-
-
+        Person person5 = new Person("Yaroslav","Samartcev");
+        assertEquals(person.getFirstName(),"Dasha");
+        assertEquals(person1.getFirstName(),"Alexandr");
+        assertEquals(person2.getFirstName(),"Natali");
+        assertEquals(person3.getFirstName(),null);
+        //assertEquals(person4.getFirstName(),"a");
+        assertEquals(person5.getFirstName(),"Yaroslav");
+    }
+    @Test
+    public void testLastName(){
+        Person person = new Person();
+        person.setLastName("Beethoven");
+        Person person1 = new Person("James","Cook");
+        Person person2 = new Person("Charles","Darwin",226);
+        Person person3=new Person();
+        person3.setLastName(null);
+        assertEquals(person.getLastName(),"Beethoven");
+        assertEquals(person1.getLastName(),"Cook");
+        assertEquals(person2.getLastName(),"Darwin");
+        assertEquals(person3.getLastName(),null);
+    }
+    @Test
+    public void testPassportId(){
+        Person person = new Person();
+        person.setPasssportId(1234);
+        Person person1 = new Person(123);
+        Person person2 = new Person("Charles","Darwin",226);
+        assertEquals(person.getPasssportId(),1234);
+        assertEquals(person1.getPasssportId(),123);
+        assertEquals(person2.getPasssportId(),226);
     }
 }
