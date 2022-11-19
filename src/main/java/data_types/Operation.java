e263852a38d83b5680e950b5c6d83aff173e331c
Murashkina <grewytri33@gmail.com>
2022-11-19 22:06:53 +0400

    updated Point, added class Operation and OperationTest

---
+++ src/main/java/data_types/Operation.java
@@ -0,0 +1,26 @@
+package data_types;
+
+public abstract class Operation {
+    public abstract double apply(double number);
+    public double applyTriple(double number){
+        return apply(apply(apply(number)));
+    }
+}
+class IdenticalOperation extends Operation{
+    @Override
+    public double apply(double number) {
+        return number;
+    }
+}
+class SqrtOperation extends Operation{
+    @Override
+    public double apply(double number) {
+        return Math.sqrt(number);
+    }
+}
+class TangentOperation extends Operation{
+    @Override
+    public double apply(double number) {
+        return Math.tan(number);
+    }
+}
---
+++ src/main/java/data_types/OperationTest.java
@@ -0,0 +1,16 @@
+package data_types;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class OperationTest {
+
+    @Test
+    public void testApply() {
+    }
+
+    @Test
+    public void testApplyTriple() {
+    }
+}
